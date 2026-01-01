ead4556721c182d6ff3f76efc33769d16c716f9b
Ben <bennylazarussonu@gmail.com>
2026-01-01 11:35:06 +0530

    Configured Axios for both localhosting and Vercel hosting

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
+// const api = axios.create({
+//   // baseURL: "http://localhost:5000"
+// })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
