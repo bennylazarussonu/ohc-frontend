b4438e3701b6354d3378f53c4269d9aae21f8cf7
bennylazarussonu <63468822+bennylazarussonu@users.noreply.github.com>
2026-01-07 10:04:40 +0530

    Axios bug(set from localhost to production backend)
    Updated axios API configuration to use environment variable for base URL.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL
-// });
 const api = axios.create({
-  baseURL: "http://localhost:5000"
-})
+  baseURL: import.meta.env.VITE_API_URL
+});
+// const api = axios.create({
+//  baseURL: "http://localhost:5000"
+// })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
