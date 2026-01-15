8ab91b8abcaf59f3ed3e37f7434052f776075dda
Ben <bennylazarussonu@gmail.com>
2026-01-15 17:43:46 +0530

    *

--- src/App.jsx
+++ src/App.jsx
@@ -1090,38 +1090,38 @@ const effectiveDoctorId =
         )}
 
         {activeMenu === "dispensary" && (
-          <BUListUpload/>
-          // <div className='w-full'>
-          //   <div className="w-full flex bg-gray-800 p-2 rounded gap-2">
-          //     <div className={`bg-gray-700 w-1/3 rounded `}>
-          //       <button className={`text-sm p-1 text-center rounded font-semibold w-full ${dispensaryTab === "procurement" ? ("bg-blue-600") : ("")}`} onClick={() => setDispensaryTab("procurement")}>Procurement</button>
-          //     </div>
-          //     <div className="bg-gray-700 w-1/3 rounded">
-          //       <button className={`text-sm p-1 text-center font-semibold w-full rounded ${dispensaryTab === "dispense" ? ("bg-blue-600") : ("")}`} onClick={() => setDispensaryTab("dispense")}>Dispense Medicine</button>
-          //     </div>
-          //     <div className="bg-gray-700 w-1/3 rounded">
-          //       <button className={`text-sm p-1 text-center font-semibold w-full rounded ${dispensaryTab === "stock" ? ("bg-blue-600") : ("")}`} onClick={() => setDispensaryTab("stock")}>Stock</button>
-          //     </div>
-          //   </div>
-          //   {dispensaryTab === "procurement" && (
-          //     <div>
-          //       <h2 className='text-sm font-bold mt-4'>PROCUREMENT</h2>
-          //       <Procurement />
-          //     </div>
-          //   )}
-          //   {dispensaryTab === "dispense" && (
-          //     <div>
-          //       <h2 className='text-sm font-bold mt-4'>DISPENSE</h2>
-          //       <Dispense />
-          //     </div>
-          //   )}
-          //   {dispensaryTab === "stock" && (
-          //     <div>
-          //       <h2 className='text-sm font-bold mt-4'>STOCK</h2>
-          //       <Stock />
-          //     </div>
-          //   )}
-          // </div>
+          // <BUListUpload/>
+          <div className='w-full'>
+            <div className="w-full flex bg-gray-800 p-2 rounded gap-2">
+              <div className={`bg-gray-700 w-1/3 rounded `}>
+                <button className={`text-sm p-1 text-center rounded font-semibold w-full ${dispensaryTab === "procurement" ? ("bg-blue-600") : ("")}`} onClick={() => setDispensaryTab("procurement")}>Procurement</button>
+              </div>
+              <div className="bg-gray-700 w-1/3 rounded">
+                <button className={`text-sm p-1 text-center font-semibold w-full rounded ${dispensaryTab === "dispense" ? ("bg-blue-600") : ("")}`} onClick={() => setDispensaryTab("dispense")}>Dispense Medicine</button>
+              </div>
+              <div className="bg-gray-700 w-1/3 rounded">
+                <button className={`text-sm p-1 text-center font-semibold w-full rounded ${dispensaryTab === "stock" ? ("bg-blue-600") : ("")}`} onClick={() => setDispensaryTab("stock")}>Stock</button>
+              </div>
+            </div>
+            {dispensaryTab === "procurement" && (
+              <div>
+                <h2 className='text-sm font-bold mt-4'>PROCUREMENT</h2>
+                <Procurement />
+              </div>
+            )}
+            {dispensaryTab === "dispense" && (
+              <div>
+                <h2 className='text-sm font-bold mt-4'>DISPENSE</h2>
+                <Dispense />
+              </div>
+            )}
+            {dispensaryTab === "stock" && (
+              <div>
+                <h2 className='text-sm font-bold mt-4'>STOCK</h2>
+                <Stock />
+              </div>
+            )}
+          </div>
         )}
       </div>
     </>
