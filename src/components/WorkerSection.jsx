97b3ee5dae2967cf43b61b1e1ba0762dd0bb2b06
Ben <bennylazarussonu@gmail.com>
2026-03-09 19:03:03 +0530

    *

--- src/components/WorkerSection.jsx
+++ src/components/WorkerSection.jsx
@@ -37,7 +37,7 @@ function WorkerSection({ search, results, onSearch, onSelect }) {
                 {w.name}
                 <br />
                 <span className="font-light text-sm">
-                  {w.fathers_name} | Emp ID: {w.employee_id} | 📞 {w.phone_no} | Contractor Name: {w.contractor_name} | Blood Group: {w.blood ? `${w.blood.group} ${w.blood.rh_factor}` : ""}
+                  {w.fathers_name} | Emp ID: {w.employee_id} | 📞 {w.phone_no} | Contractor Name: {w.contractor_name} | Blood Group: {w.blood ? `${w.blood?.group} ${w.blood?.rh_factor}` : ""}
                   <br />
                   DOJ: {w.date_of_joining
                     ? formatISTDate(w.date_of_joining)
